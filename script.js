cac146515445cdc431fff11f469cd49dd1692745
DevakirubaiJohn <johndevakirubai21@gmail.com>
2024-08-04 12:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const byteSize = (str) => {
   // write your code here
+	 // Create a new Blob object with the inputString
+  let blob = new Blob([str], {type : "text/plain"});
+  
+  // Use the size property of the Blob object to get the size of the string in bytes
+  let size = blob.size;
+  
+  // Return the size
+  return size;
 };
+
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
